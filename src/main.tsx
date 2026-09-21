a7e7415369a15a3eae8d65778327ccad360aef45
chandrabl <chandrakrbl73@gmail.com>
2026-09-21 15:16:59 +0530

    fix: disable eslint no-explicit-any on main.tsx polyfills

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,9 @@ import "./styles/index.css";
 import { Buffer } from "buffer";
 
 if (typeof window !== "undefined") {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (window as any).Buffer = (window as any).Buffer || Buffer;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (window as any).process = (window as any).process || { env: {} };
 }
 
